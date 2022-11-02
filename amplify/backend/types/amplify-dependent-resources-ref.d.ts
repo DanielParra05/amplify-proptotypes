200da5f8f48d2f822f657cec37aa20b8c5b7e9e7
Jose Daniel Parra Gonzalez <jose-paper@hotmail.com>
2022-11-02 13:44:58 -0500

    Add amplify and GraphQL API

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "vuetemplatevite": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
