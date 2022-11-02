b7ca4a38119b1e89c9b5541a977500587bdf5e89
Jose Daniel Parra Gonzalez <jose-paper@hotmail.com>
2022-11-02 14:05:26 -0500

    Amplify add API

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,58 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from '@vue/reactivity';
+import { API } from 'aws-amplify';
+import { onMounted } from 'vue';
+import { createTodo } from './graphql/mutations';
+import { listTodos } from './graphql/queries';
+import { onCreateTodo } from './graphql/subscriptions';
+
+const name = ref('');
+const description = ref('');
+const todos = ref<any[]>([]);
+
+async function insertTodo() {
+  if (!name.value || !description.value) return;
+  const todo = { name, description };
+  await API.graphql({
+    query: createTodo,
+    variables: { input: todo }
+  });
+}
+
+async function getTodos() {
+  const todos = await API.graphql({
+    query: listTodos
+  });
+  todos.value = todos.data.listTodos.items;
+}
+
+function subscribe() {
+  API.graphql({ query: onCreateTodo }).subscribe({
+    next: (eventData: any) => {
+      let todo = eventData.value.data.onCreateTodo;
+      if (todos.value.some((item) => item.name === todo.name)) return; // remove duplications
+      todos.value = [...todos.value, todo];
+    }
+  });
+}
+
+onMounted(() => {
+  getTodos();
+  subscribe();
+});
 </script>
 
 <template>
-  
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <router-view />
+  <div id="app">
+    <h1>Todo App</h1>
+    <input type="text" v-model="name" placeholder="Todo name" />
+    <input type="text" v-model="description" placeholder="Todo description" />
+    <button v-on:click="insertTodo">Create Todo</button>
+  </div>
+  <div v-for="item in todos" :key="item.id">
+    <h3>{{ item.name }}</h3>
+    <p>{{ item.description }}</p>
+  </div>
 </template>
 
 <style>
